6e6c867dbe854b20c3ffbcd959ca0595c8480fb5
Bogachev Dmitrii <bogachevx@gmail.com>
2019-07-01 11:47:14 +0300

    Another update

--- FotoRobot/Properties/Resources.Designer.cs
+++ FotoRobot/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FotoRobot.Properties {
     // с помощью такого средства, как ResGen или Visual Studio.
     // Чтобы добавить или удалить член, измените файл .ResX и снова запустите ResGen
     // с параметром /str или перестройте свой проект VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -77,6 +77,16 @@ namespace FotoRobot.Properties {
             get {
                 object obj = ResourceManager.GetObject("icon", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_робот_filled_100__1_ {
+            get {
+                object obj = ResourceManager.GetObject("icons8_робот_filled_100__1_", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
